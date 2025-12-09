ae40b5c54ac23219a7e0b5bb0e04cc6940926ac7
Nguyễn Cao Phúc <nguyencaophuc.204@gmail.com>
2025-12-09 16:49:56 +0700

    feat(auth): Create LogoutUseCase for handling logout logic

--- app/src/main/java/com/example/app_go_play/feature/auth/domain/usecase/LogoutUseCase.kt
+++ app/src/main/java/com/example/app_go_play/feature/auth/domain/usecase/LogoutUseCase.kt
@@ -1,11 +1,14 @@
 package com.example.app_go_play.feature.auth.domain.usecase
 
 import com.example.app_go_play.feature.auth.domain.repository.AuthRepository
+import javax.inject.Inject
 
-class LogoutUseCase(private val authRepository: AuthRepository) {
-
+class LogoutUseCase @Inject constructor(
+    private val repository: AuthRepository
+) {
     suspend operator fun invoke() {
-        authRepository.logout()
+        // Simply call the logout function from the repository.
+        // The repository is responsible for handling API calls and exceptions.
+        repository.logout()
     }
-
 }
